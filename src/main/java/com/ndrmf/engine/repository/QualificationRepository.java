b13a1abc250b161dc30609c37bef064c01b911d4
adilkhalil <adilkhalil@outlook.com>
2020-05-08 21:45:27 +0500

    query bug fix

--- src/main/java/com/ndrmf/engine/repository/QualificationRepository.java
+++ src/main/java/com/ndrmf/engine/repository/QualificationRepository.java
@@ -14,9 +14,9 @@ public interface QualificationRepository extends JpaRepository<Qualification, UU
 	@Query(value = "SELECT COUNT(q) FROM Qualification q JOIN q.initiatedBy ib WHERE ib.id = :userId AND q.status IN (:statuses)")
 	int checkCountForUserWithStatuses(@Param("userId") UUID userId, @Param("statuses") Set<String> statuses);
 	
-	@Query(value = "SELECT q FROM Qualification q JOIN q.processOwner po JOIN e.initiatedBy ib WHERE (po.id = :userId OR ib.id = :userId) AND q.status = :status")
+	@Query(value = "SELECT q FROM Qualification q JOIN q.processOwner po JOIN q.initiatedBy ib WHERE (po.id = :userId OR ib.id = :userId) AND q.status = :status")
 	List<Qualification> findRequestsForOwnerOrInitiatorByStatus(@Param("userId") UUID userId, @Param("status") String status);
 	
-	@Query(value = "SELECT q FROM Qualification q JOIN q.processOwner po JOIN e.initiatedBy ib WHERE po.id = :userId OR ib.id = :userId")
+	@Query(value = "SELECT q FROM Qualification q JOIN q.processOwner po JOIN q.initiatedBy ib WHERE po.id = :userId OR ib.id = :userId")
 	List<Qualification> findAllRequestsForOwnerOrInitiator(@Param("userId") UUID userId);
 }
